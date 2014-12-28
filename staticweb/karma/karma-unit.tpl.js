50769ce6eb01e6a4867a02bcd67a00c8a4252144
Eric Huang <wongngaikei@gmail.com>
2014-12-27 22:29:39 -0600

    use phantomjs instead

--- staticweb/karma/karma-unit.tpl.js
+++ staticweb/karma/karma-unit.tpl.js
@@ -18,7 +18,7 @@ module.exports = function ( karma ) {
       'src/assets/**/*.js'
     ],
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-coffee-preprocessor', 'karma-junit-reporter' ],
+    plugins: [ 'karma-jasmine', 'karma-phantomjs-launcher', 'karma-coffee-preprocessor', 'karma-junit-reporter' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
@@ -59,7 +59,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      'PhantomJS'
     ]
   });
 };
