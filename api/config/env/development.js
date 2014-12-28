a59812976c68ed39535c8ce9f84a555b5baea526
Eric Huang <wongngaikei@gmail.com>
2014-12-28 01:12:05 -0600

    tweaked environments so they will work locally

--- api/config/env/development.js
+++ api/config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'mongodbServer'
+    connection: 'localDiskDb'
   }
 
 };
